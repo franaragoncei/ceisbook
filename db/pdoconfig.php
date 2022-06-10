253acfec2f3fb0c6796918c6cb3a6f164794e834
CEI <admin@41.local>
2022-06-10 18:23:09 +0200

    Cambia configuración bbdd

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$host = 'localhost';
-$dbname = 'ceisbook';
-$username = 'fran';
-$password = '1234';
+$host = 'eu-cdbr-west-02.cleardb.net';
+$dbname = 'heroku_db043b0b6d07824';
+$username = 'b0af8976d02828';
+$password = 'e1cee0e9';
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
